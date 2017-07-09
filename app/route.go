d0efc0e634ed212efa6a6a718ca15a580a72c945
harymindiar <mindiar@yahoo.com>
2017-07-09 23:51:02 +0700

    add built-in middleware and use negroni as middleware http

--- app/route.go
+++ app/route.go
@@ -3,15 +3,25 @@ package app
 import (
 	"github.com/gorilla/mux"
 	"github.com/harymindiar/mini/handler"
+	"github.com/harymindiar/mini/middleware"
+	"github.com/urfave/negroni"
 	"net/http"
 )
 
 // Handlers here you need to register all the route of application
 func (a *Application) Handlers() http.Handler {
-	router := mux.NewRouter()
-
 	baseHandler := handler.NewHandler(a.Container)
-	router.HandleFunc("/", baseHandler.Index)
 
-	return router
+	r := mux.NewRouter()
+	r.HandleFunc("/", baseHandler.Index)
+
+	// default middleware
+	n := negroni.New(negroni.NewRecovery())
+	if a.IsDevelopment() {
+		n.Use(middleware.NewLogger())
+	}
+
+	n.UseHandler(r)
+
+	return n
 }
